ddb1ae7a71394d3491b81ad5a221ab0d4b915728
Bregy Malpartida <bregy@minsky.cc>
2023-05-06 00:48:54 -0500

    feat: update pi example

--- examples/pi.rs
+++ examples/pi.rs
@@ -1,7 +1,7 @@
 use auto_rust::auto_implement;
 
 #[auto_implement]
-#[doc = "This function calculates pi with a robust and very fast algorithm."]
+/// This function calculates pi with a robust and very fast algorithm.
 fn calculate_pi_with_n_decimals(n: u32) -> f64 {
     todo!()
 }
@@ -9,4 +9,5 @@ fn calculate_pi_with_n_decimals(n: u32) -> f64 {
 fn main() {
     let result = calculate_pi_with_n_decimals(10);
     println!("pi: {}", result);
+    // assert_eq!(result, 3.0418396189294032);
 }
