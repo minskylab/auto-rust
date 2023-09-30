45ec427be1ac8df8f03c99d64d8ee225b6ef0b83
Bregy Malpartida <bregy@minsky.cc>
2023-09-30 01:38:40 -0500

    wip: cargo-self as a cargo wrapper with smart features to extend the way to program rust with LLMs

--- tools/cargo-self/src/main.rs
+++ tools/cargo-self/src/main.rs
@@ -1,3 +1,21 @@
+use std::path::Path;
+
+use cargo::ops::compile;
+
 fn main() {
     println!("Hello, world!");
+
+    let manifest_path = Path::new("Cargo.toml");
+    let config = cargo::util::config::Config::default().unwrap();
+    let ws = cargo::core::Workspace::new(manifest_path, &config).unwrap();
+
+    let options =
+        cargo::ops::CompileOptions::new(&config, cargo::core::compiler::CompileMode::Build)
+            .unwrap();
+
+    let res = compile(&ws, &options).unwrap();
+
+    res.binaries.iter().for_each(|bin| {
+        println!("bin: {}", bin.path.display());
+    });
 }
