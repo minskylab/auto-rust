f85e33d2cd1e61cc68fe916478b39328e6fbd623
Bregy Malpartida <bregy@minsky.cc>
2023-10-04 17:36:40 -0500

    feat: cargo self compile itself jaja

--- tools/cargo-self/src/main.rs
+++ tools/cargo-self/src/main.rs
@@ -1,13 +1,26 @@
-use std::path::Path;
+use std::{fs::canonicalize, path::PathBuf};
 
-use cargo::ops::compile;
+use cargo::{core::Shell, ops::compile};
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Cargo Self v0.2.0!");
 
-    let manifest_path = Path::new("Cargo.toml");
-    let config = cargo::util::config::Config::default().unwrap();
-    let ws = cargo::core::Workspace::new(manifest_path, &config).unwrap();
+    // let manifest_path_buf = PathBuf::from("./Cargo.toml");
+    let manifest_path = canonicalize(PathBuf::from("./Cargo.toml")).unwrap();
+    // let a = Box::new(std::io::stdout());
+    // let buf = BufWriter::new(Vec::new());
+    // Shell::;
+
+    // let shell = Shell::from_write(Box::new(buf));
+    // let shell = Shell::default();
+    let config =
+        // cargo::util::config::Config::new(shell, manifest_path.to_owned(), manifest_path.to_owned());
+        cargo::util::config::Config::default().unwrap();
+
+    // config.registry_cache_path();
+
+    // cargo::core::Workspace::new(manifest_path, config)
+    let ws = cargo::core::Workspace::new(&manifest_path, &config).unwrap();
 
     let options =
         cargo::ops::CompileOptions::new(&config, cargo::core::compiler::CompileMode::Build)
